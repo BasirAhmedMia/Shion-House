731f6a3efde48613cc345cde6b371a0a825bca86
Basir Ahmed Mia <116313650+BasirAhmedMia@users.noreply.github.com>
2022-12-27 20:36:00 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+  $(".slick-slider").slick({
+   slidesToShow: 1,
+   infinite:false,
+   slidesToScroll: 1,
+   autoplay: false,
+   autoplaySpeed: 2000
+   dots: true,
+   infinite: true,
+   speed: 500,
+   fade: true,
+   cssEase: 'linear'
+
+     // dots: false, Boolean
+    // arrows: false, Boolean
+  });
